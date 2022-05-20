b353f5be0e79c5faf89a6d9096666d696145dd79
Tamer Kanak <81855382+iamtamerr@users.noreply.github.com>
2022-05-20 11:26:17 +0300

    Add files via upload

---
+++ C/Projelerim/element_reverser.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	
+	int i,n,j;
+	
+	printf("Number of Elements: ");
+	scanf("%d",&n);
+	
+	int dizi[n];
+	
+	
+	for(i=0;i<n;i++)
+	{
+		dizi[i] = i+1;
+		printf("%d",dizi[i]);			
+	}
+	
+	printf("\n");	
+	
+	for(j=1 ;j<n;j++)
+	{
+		
+		for(i=0;i<n;i++)
+		{
+			if(dizi[i]==n)
+			{
+				dizi[i] = 1;	
+			}
+			else
+				dizi[i] = dizi[i]+1;
+			printf("%d",dizi[i]);
+									
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
